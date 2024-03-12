4bc952ff9b767d2583cbed792583fd87256569a6
serkanboyraci <serkanboyraci@gmail.com>
2024-03-12 17:58:34 +0100

    tabBarController added

--- InstagramCloneApp/ViewController.swift
+++ InstagramCloneApp/ViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil )
+    }
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
 }
 
