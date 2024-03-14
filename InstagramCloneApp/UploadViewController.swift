c7a09186c7eefac2015e4f5ea62b4b49c7fd729b
serkanboyraci <serkanboyraci@gmail.com>
2024-03-14 14:38:57 +0100

    Update UploadViewController.swift

--- InstagramCloneApp/UploadViewController.swift
+++ InstagramCloneApp/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -39,6 +41,30 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
 
   
     @IBAction func uploadButtonClicked(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                } else {
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+                
+            
+            }
+        }
+        
     }
     
 }
