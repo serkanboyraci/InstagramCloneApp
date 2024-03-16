b4df2d976e8c0d4614ddc7b941057b8268436129
serkanboyraci <serkanboyraci@gmail.com>
2024-03-16 13:08:08 +0100

    feedcell added

---
+++ InstagramCloneApp/FeedCell.swift
@@ -0,0 +1,32 @@
+//
+//  FeedCell.swift
+//  InstagramCloneApp
+//
+//  Created by Ali serkan Boyracı  on 16.03.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func likeButtonClicked(_ sender: Any) {
+        
+        
+    }
+}
