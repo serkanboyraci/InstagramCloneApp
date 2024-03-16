55608945094e5e15d8f1dba1c3c335ba62460379
serkanboyraci <serkanboyraci@gmail.com>
2024-03-16 13:17:16 +0100

    Feedcell updated

--- InstagramCloneApp/FeedViewController.swift
+++ InstagramCloneApp/FeedViewController.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
+        
+        return cell
+    }
 }
