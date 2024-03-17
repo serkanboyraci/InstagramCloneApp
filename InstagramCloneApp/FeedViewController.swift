3dd280a682c4d7b9a5721de5b7de54092d9eeaab
serkanboyraci <serkanboyraci@gmail.com>
2024-03-18 00:27:39 +0100

    Update FeedViewController.swift

--- InstagramCloneApp/FeedViewController.swift
+++ InstagramCloneApp/FeedViewController.swift
@@ -12,11 +12,19 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var tableView: UITableView!
     
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getDataFromFirestore()
 
         // Do any additional setup after loading the view.
     }
@@ -34,7 +42,26 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             if error != nil {
                 print(error?.localizedDescription)
             } else {
-                
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    for document in snapshot!.documents { // snpashot gives us array. we must take one by one
+                        
+                        let documentID = document.documentID
+                        
+                        if let postedBy = document.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postComment = document.get("postComment") as? String {
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            self.userImageArray.append(imageUrl)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
             }
         }
         
@@ -48,14 +75,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
 
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.userImageView.image = UIImage(named: "select.png")
         return cell
     }
 }
